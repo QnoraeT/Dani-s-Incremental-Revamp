09569fa1f4729e57f10b3363edc2f78ea3d50476
QnoraeT <xxajlopezxx@gmail.com>
2024-02-28 17:24:31 -0500

    the screen FREEZES AAAA

--- src/utils/vueSetup.js
+++ src/utils/vueSetup.js
@@ -1,28 +1,26 @@
 "use strict";
-var app;
-
 function loadVue() {
-	app = new Vue({
-	    el: "#app",
-	    data: {
-            Decimal,
-            game,
-            player,
-            tmp,
-			format,
-            formatTime,
-            switchTab,
-            TABS_LIST,
-            tab,
-            otherGameStuffIg,
-            DEFAULT_SCALE,
-            gRC,
-            colorChange,
-            buyGenUPG,
-            reset,
-            ACHIEVEMENT_DATA,
-            ACH_DEF_COLORS,
-            c
-        }
-	})
+    Vue.createApp({
+        data() {
+            return { 
+                Decimal,
+                game,
+                player,
+                tmp,
+                format,
+                formatTime,
+                switchTab,
+                TABS_LIST,
+                tab,
+                otherGameStuffIg,
+                DEFAULT_SCALE,
+                gRC,
+                colorChange,
+                buyGenUPG,
+                reset,
+                ACHIEVEMENT_DATA,
+                ACH_DEF_COLORS,
+                c
+            }
+    }}).mount('#app')
 }
